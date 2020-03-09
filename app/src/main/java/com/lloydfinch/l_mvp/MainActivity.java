f2804f3f18cffd26bc15489c3d6b66aa91e01c2c
LloydFinch <wangwenqi@orangelab.cn>
2020-03-09 19:32:37 +0800

    add some lifecycle method

--- app/src/main/java/com/lloydfinch/l_mvp/MainActivity.java
+++ app/src/main/java/com/lloydfinch/l_mvp/MainActivity.java
@@ -1,14 +1,69 @@
 package com.lloydfinch.l_mvp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //<editor-fold desc="lifecycle">
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init(this);
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="init方法">
+    private void init(Context context) {
+        initView();
+        initData();
+        initEvent();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initEvent() {
+
+    }
+    //</editor-fold>
 }
